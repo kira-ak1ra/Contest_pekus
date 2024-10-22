c6b9de5e7cd99bd771fb9b3646fe93640b9b4398
Shestakov Demid <demid873@gmail.com>
2024-10-22 10:17:06 +0500

    O (kira-ak1ra).cpp

--- Source/Contest4/O.cpp
+++ Source/Contest4/O.cpp
@@ -1,60 +1,41 @@
 #include <iostream>
 
+int StrSize(char str[]) {
+  int answ = 1;
+  while (str[answ] != 0) {
+    ++answ;
+  }
+  return answ;
+}
+
+bool IsAnagram(char str_1[], char str_2[]) {
+  if (StrSize(str_1) != StrSize(str_2)) {
+    return false;
+  }
+  bool answ = true;
+  int symb_count_1[26] = {};
+  int symb_count_2[26] = {};
+  for (int i = StrSize(str_1) - 1; i >= 0; --i) {
+    ++symb_count_1[str_1[i] - 97];
+    ++symb_count_2[str_2[i] - 97];
+  }
+  for (int i = 0; i < 26; ++i) {
+    if (symb_count_1[i] != symb_count_2[i]) {
+      answ = false;
+      break;
+    }
+  }
+  return answ;
+}
+
 int main() {
-  char* s1 = new char[1000000];
-  char* s2 = new char[1000000];
-
-  std::cin.getline(s1, 1000000);
-  std::cin.getline(s2, 1000000);
-
-  int len1 = 0;
-  int len2 = 0;
-
-  for (int i = 0; i < 1000000; i++) {
-    if (s1[i] != 0) {
-      len1++;
-    } else {
-      break;
-    }
-  }
-
-  for (int i = 0; i < 1000000; i++) {
-    if (s2[i] != 0) {
-      len2++;
-    } else {
-      break;
-    }
-  }
-
-  if (len1 != len2) {
+  char a[1000000] = {};
+  char b[1000000] = {};
+  std::cin >> a >> b;
+  if (IsAnagram(a, b)) {
+    std::cout << "YES";
+  } else {
     std::cout << "NO";
-    return 0;
   }
-
-  int letters1[256] = { 0 };
-  int letters2[256] = { 0 };
-
-  for (int i = 0; i < len1; i++) {
-    int pos = s1[i];
-    letters1[pos] += 1;
-  }
-
-  for (int i = 0; i < len2; i++) {
-    int pos = s2[i];
-    letters2[pos] += 1;
-  }
-
-  for (int i = 0; i < 256; i++) {
-    if (letters1[i] != letters2[i]) {
-      std::cout << "NO";
-      delete[] s1;
-      delete[] s2;
-      return 0;
-    }
-  }
-
-  std::cout << "YES";
-
-  delete[] s1;
-  delete[] s2;
+  return 0;
 }
